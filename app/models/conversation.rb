8ea246f8537d0f1f00f503a1deb03459e405fabb
icarus96119 <icarus96119@gmail.com>
2020-07-13 04:42:03 +0900

    feat: add validation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,7 @@ class Conversation < ApplicationRecord
   belongs_to :request
   belongs_to :user
   belongs_to :message
+  validates :request_id, presence: true
+  validates :user_id, presence: true
+  validates :message_id, presence: true, uniqueness: true
 end
